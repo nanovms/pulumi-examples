929302af86c00a2dd966e6a02cd53e4e69d81a48
Ian Eyberg <ian@nanovms.com>
2025-10-13 10:50:56 -0700

    adding in initial pulumi example

---
+++ azure/index.ts
@@ -0,0 +1,83 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as storage from "@pulumi/azure-native/storage";
+import * as azure_native from "@pulumi/azure-native";
+
+const config = new pulumi.Config();
+
+// set the following:
+const prefix = "myimg";
+const location = "WestUS2";
+const resourceGroup = "myregion";
+const subId = "my-subscription-id-goes-here";
+const bucket = "mybucket";
+const galleryName = "gallery-name";
+
+// Create a Virtual Network
+const virtualNetwork = new azure_native.network.VirtualNetwork("vm-vnet", {
+    addressSpace: {
+      addressPrefixes: ["10.0.0.0/16"],
+    },
+    location: location,
+    resourceGroupName: resourceGroup,
+});
+
+// Create a Subnet
+const subnet = new azure_native.network.Subnet("vm-subnet", {
+    name: "default",
+    resourceGroupName: resourceGroup,
+    virtualNetworkName: virtualNetwork.name,
+    addressPrefixes: ["10.0.1.0/24"],
+});
+
+// Create a Network Interface
+const networkInterface = new azure_native.network.NetworkInterface("vm-nic", {
+    location: location,
+    resourceGroupName: resourceGroup,
+    ipConfigurations: [{
+        name: "ipconfig1",
+        subnet: {
+          id: subnet.id,
+        },
+    }],
+});
+
+// Create the Virtual Machine
+const virtualMachine = new azure_native.compute.VirtualMachine("vm", {
+    vmName: `${prefix}-vm`,
+    location: location,
+    resourceGroupName: resourceGroup,
+    diagnosticsProfile: {
+        bootDiagnostics: {
+            enabled: true,
+            storageUri: "http://" + bucket + ".blob.core.windows.net",
+        },
+    },
+    networkProfile: {
+        networkInterfaces: [{
+            id: "/subscriptions/" + subId + "/resourceGroups/" + resourceGroup + "/providers/Microsoft.Network/networkInterfaces/" + prefix + "-nic",
+            primary: true,
+        }],
+    },
+    hardwareProfile: {
+        vmSize: azure_native.compute.VirtualMachineSizeTypes.Standard_D2s_v3,
+    },
+    storageProfile: {
+      imageReference: {
+        id: "/subscriptions/" + subId + "/resourceGroups/" + resourceGroup + "/providers/Microsoft.Compute/galleries/" + galleryName + "/images/" + prefix,
+      },
+      osDisk: {
+            caching: azure_native.compute.CachingTypes.ReadWrite,
+            createOption: azure_native.compute.DiskCreateOptionTypes.FromImage,
+            managedDisk: {
+                storageAccountType: azure_native.compute.StorageAccountTypes.Premium_LRS,
+            },
+            name: "myVMosdisk2",
+        },
+    },
+    osProfile: {
+        computerName: "myvm",
+        adminUsername: "pulumiadmin",
+        adminPassword: "some-random-pass-that-is-not-used",
+    },
+});
